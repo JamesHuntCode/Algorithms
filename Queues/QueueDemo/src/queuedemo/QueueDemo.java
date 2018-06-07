b84ec766997dd27b847ff81798c6fb6c47ebe6b1
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-07 23:27:10 +0100

    pushed blank files for queue examples

---
+++ Queues/QueueDemo/src/queuedemo/QueueDemo.java
@@ -0,0 +1,10 @@
+package queuedemo;
+
+public class QueueDemo {
+
+    /** program to demonstrate the implementation of a queue data structure. **/
+    public static void main(String[] args) {
+        
+    }
+    
+}
