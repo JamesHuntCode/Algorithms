9d35e24db9a72b71be6b0ad9d511ef552c1d8790
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-07 23:30:33 +0100

    removed licensing from .java files where applicable

--- Linked Lists/LinkedListDemo/src/linkedlistdemo/LinkedListDemo.java
+++ Linked Lists/LinkedListDemo/src/linkedlistdemo/LinkedListDemo.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package linkedlistdemo;
 
 /** Program to demonstrate an implementation of a dynamic linked list **/
