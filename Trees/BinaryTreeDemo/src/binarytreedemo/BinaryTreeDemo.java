1ad5a6d5f359b73940465e415e1eb8a5f6dde0c4
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-12 00:55:46 +0100

    making headway for adding to binary search tree data structure

--- Trees/BinaryTreeDemo/src/binarytreedemo/BinaryTreeDemo.java
+++ Trees/BinaryTreeDemo/src/binarytreedemo/BinaryTreeDemo.java
@@ -21,25 +21,25 @@ public class BinaryTreeDemo {
         myTree.breadthFirstTraversal();
         
         // Remove an element from the tree.
-        System.out.print("\nNode with value 5 removed from the tree.\n\n");
-        myTree.removeNode(new Node(5));
+        //System.out.print("\nNode with value 5 removed from the tree.\n\n");
+        //myTree.removeNode(new Node(5));
         
         // Traverse tree.
-        System.out.print("\nDepth first tree traversal example:\n\n");
-        myTree.depthFirstTraversal();
+        //System.out.print("\nDepth first tree traversal example:\n\n");
+        //myTree.depthFirstTraversal();
         
         // Search the tree.
-        boolean valueLocated = ((myTree.search(new Node(17)).value) != -1);
-        System.out.print("\nDoes the tree contain a 17? " + valueLocated + ".\n\n");
+        //boolean valueLocated = ((myTree.search(new Node(17)).value) != -1);
+        //System.out.print("\nDoes the tree contain a 17? " + valueLocated + ".\n\n");
         
         // Is the tree empty?
-        System.out.print("Is the tree empty? " + myTree.isEmpty() + ".\n\n");
+        //System.out.print("Is the tree empty? " + myTree.isEmpty() + ".\n\n");
         
         // How many nodes inside the tree?
-        System.out.print("How many nodes are in the tree? " + myTree.nodeCount() + ".\n\n");
+        //System.out.print("How many nodes are in the tree? " + myTree.nodeCount() + ".\n\n");
         
         // What's the sum of all Node data in the tree?
-        System.out.print("What is the sum of all node data in the tree? " + myTree.sum() + ".\n\n");
+        //System.out.print("What is the sum of all node data in the tree? " + myTree.sum() + ".\n\n");
         
     }
     
@@ -64,13 +64,55 @@ class BinarySearchTree
         {
             rootNode = newNode;
         }
+        else if (rootNode.left == null || rootNode.right == null)
+        {
+            if (newNode.value == rootNode.value)
+            {
+                System.out.print("Value already exists in the tree. Cannot add element.");
+            }
+            else if (newNode.value > rootNode.value)
+            {
+                rootNode.right = newNode;
+            }
+            else 
+            {
+                rootNode.left = newNode;
+            }
+        }
         else 
         {
             Node current = rootNode;
-        
+            int currentValue = current.value;
+            int newValue = newNode.value;
+            
             while ((current.left != null) && (current.right != null))
             {
-                
+                if (newValue == currentValue)
+                {
+                    System.out.print("Value already exists in the tree. Cannot add element.");
+                }
+                else if (newValue > currentValue)
+                {
+                    if (current.right == null)
+                    {
+                        current.right = newNode;
+                    }
+                    else 
+                    {
+                        current = current.right;
+                    }
+                }
+                else if (newValue < currentValue)
+                {
+                    if (current.left == null)
+                    {
+                        current.left = newNode;
+                    }
+                    else 
+                    {
+                        current = current.left;
+                    }
+                }
             }
         }
     }
@@ -88,18 +130,20 @@ class BinarySearchTree
         }
     }
     
-    /** traverse the tree implementing breadth first traversal **/
+    /** traverse the tree implementing breadth first traversal and print node data **/
     public void breadthFirstTraversal()
     {
         Node current = rootNode;
         
         while ((current.left != null) && (current.right != null))
         {
+            System.out.print(current.left.value + "\n" + current.value + "\n" + current.right.value + "\n");
             
+            current = current.left;
         }
     }
     
-    /** traverse the tree implementing depth first traversal **/
+    /** traverse the tree implementing depth first traversal and print node data **/
     public void depthFirstTraversal()
     {
         Node current = rootNode;
