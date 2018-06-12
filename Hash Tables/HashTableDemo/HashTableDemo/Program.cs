8250c26eb959398d544548cc75f2c70805fc3a57
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-12 18:01:01 +0100

    hash table - files created - initial commit

---
+++ Hash Tables/HashTableDemo/HashTableDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableDemo
+{
+    class Program
+    {
+        /// <summary>
+        /// Program to demonstrate simple implementation of a hash table in C#.NET.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
