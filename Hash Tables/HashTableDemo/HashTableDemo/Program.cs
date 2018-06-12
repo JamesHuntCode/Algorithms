a8e08918491fc30154b563135ae30ef6ed6e5a4d
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-12 18:37:40 +0100

    added all hash table functionality - planned out - working on everyting

--- Hash Tables/HashTableDemo/HashTableDemo/Program.cs
+++ Hash Tables/HashTableDemo/HashTableDemo/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace HashTableDemo
 {
+    /// <summary>
+    /// A program where the hash table contains index names.
+    /// </summary>
     class Program
     {
         /// <summary>
@@ -13,6 +16,58 @@ namespace HashTableDemo
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Hash-Table data structure.
+    /// </summary>
+    class HashTable
+    {
+        /// <summary>
+        /// Index of all possible letters in the alphabet pointing to indexes in the entries list.
+        /// </summary>
+        int[] indexes = new int[26];
+
+        /// <summary>
+        /// List of strings that indexes in the index array will point too.
+        /// </summary>
+        List<string> entries = new List<string>();
+
+        /// <summary>
+        /// Determine if the hash table contains any elements.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Return the number of elements inside the hash table.
+        /// </summary>
+        /// <returns></returns>
+        public int Size()
+        {
+            return -1;
+        }
+
+        /// <summary>
+        /// Check hash table for element, if located, return index, else, return -1.
+        /// </summary>
+        /// <param name="searchingfor"></param>
+        /// <returns></returns>
+        public int Search(string searchingfor)
+        {
+            return -1;
+        }
+
+        /// <summary>
+        /// Visit every entry of the hash table (using the index array) and log the data element to the console.
+        /// </summary>
+        public void Traverse()
         {
 
         }
