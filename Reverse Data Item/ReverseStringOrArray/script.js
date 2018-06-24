18bfd755a03a1677076838d38a1799062aee928f
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-24 21:55:34 +0100

    reverse algorithm written

--- Reverse Data Item/ReverseStringOrArray/script.js
+++ Reverse Data Item/ReverseStringOrArray/script.js
@@ -3,14 +3,26 @@
 // Method to perform the reverse of a string.
 var ReverseString = function (inputString) {
 
+    var reversedString = "";
 
+    for (let i = (inputString.length - 1); i >= 0; i--) {
+        reversedString += inputString[i];
+    }
+
+    return reversedString;
 
 }
 
 // Method to perform the reverse of an array.
 var ReverseArray = function (inputArray) {
 
+    var reversedArray = [];
 
+    for (let i = (inputArray.length - 1); i >= 0; i--) {
+        reversedArray.push(inputArray[i]);
+    }
+
+    return reversedArray;
 
 }
 
@@ -26,7 +38,7 @@ var Main = function () {
     }
 
     // String to be operated on with a random value.
-    var stringReverseMe = "12345678910";
+    var stringReverseMe = "123456789";
 
     // Print out elements before and after reversing algorithm has been called.
 
