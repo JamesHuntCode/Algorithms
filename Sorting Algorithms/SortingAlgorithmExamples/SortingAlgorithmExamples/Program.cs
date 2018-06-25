e4915efc7b62b47f8e6a82dfd07aed1bc7e6314e
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-25 23:40:24 +0100

    jumbled dataset created - all methods planned out - now implementing sorting algorithms

--- Sorting Algorithms/SortingAlgorithmExamples/SortingAlgorithmExamples/Program.cs
+++ Sorting Algorithms/SortingAlgorithmExamples/SortingAlgorithmExamples/Program.cs
@@ -14,77 +14,109 @@ namespace SortingAlgorithmExamples
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            // Log the sorted arrays & the initial dataset:
+            Console.WriteLine("Original dataset: \n\n");
 
+            int[] mixedNumbers = new int[10];
+            Random rnd = new Random();
+
+            mixedNumbers[0] = 5;
+            mixedNumbers[1] = 7;
+            mixedNumbers[2] = 1;
+            mixedNumbers[3] = 11;
+            mixedNumbers[4] = 14;
+            mixedNumbers[5] = 3;
+            mixedNumbers[6] = 2;
+            mixedNumbers[7] = 9;
+            mixedNumbers[8] = 10;
+            mixedNumbers[9] = 17;
+
+            for (int i = 0; i < mixedNumbers.Length; i++)
+            {
+                Console.Write(mixedNumbers[i].ToString() + " ");
+            }
+
+            // Different sorting methods:
+            Console.WriteLine("\n\nSelection sort: \n\n");
+            Console.WriteLine("Bubble sort: \n\n");
+            Console.WriteLine("Insertion sort: \n\n");
+            Console.WriteLine("Merge sort: \n\n");
+            Console.WriteLine("Quick sort: \n\n");
+            Console.WriteLine("Counting sort: \n\n");
+            Console.WriteLine("Heap sort: \n\n");
+
+            // Program finished.
+            Console.Read();
         }
 
         /// <summary>
         /// Selection sort.
         /// </summary>
-        /// <param name="unsortedArray"></param>
+        /// <param name="array"></param>
         /// <returns></returns>
-        static int[] SelectionSort(int[] unsortedArray)
+        static int[] SelectionSort(int[] array)
         {
-
+            return array;
         }
 
         /// <summary>
         /// Bubble sort.
         /// </summary>
-        /// <param name="unsortedArray"></param>
+        /// <param name="array"></param>
         /// <returns></returns>
-        static int[] BubbleSort(int[] unsortedArray)
+        static int[] BubbleSort(int[] array)
         {
-
+            return array;
         }
 
         /// <summary>
         /// Insertion sort.
         /// </summary>
-        /// <param name="unsortedArray"></param>
+        /// <param name="array"></param>
         /// <returns></returns>
-        static int[] InsertionSort(int[] unsortedArray)
+        static int[] InsertionSort(int[] array)
         {
-
+            return array;
         }
 
         /// <summary>
         /// Merge sort.
         /// </summary>
-        /// <param name="unsortedArray"></param>
+        /// <param name="array"></param>
         /// <returns></returns>
-        static int[] MergeSort(int[] unsortedArray)
+        static int[] MergeSort(int[] array)
         {
-
+            return array;
         }
 
         /// <summary>
         /// Quick sort.
         /// </summary>
-        /// <param name="unsortedArray"></param>
+        /// <param name="array"></param>
         /// <returns></returns>
-        static int[] QuickSort(int[] unsortedArray)
+        static int[] QuickSort(int[] array)
         {
-
+            return array;
         }
 
         /// <summary>
         /// Counting sort.
         /// </summary>
-        /// <param name="unsortedArray"></param>
+        /// <param name="array"></param>
         /// <returns></returns>
-        static int[] CountingSort(int[] unsortedArray)
+        static int[] CountingSort(int[] array)
         {
-
+            return array;
         }
 
         /// <summary>
         /// Heap sort.
         /// </summary>
-        /// <param name="unsortedArray"></param>
+        /// <param name="array"></param>
         /// <returns></returns>
-        static int[] HeapSort(int[] unsortedArray)
+        static int[] HeapSort(int[] array)
         {
-
+            return array;
         }
     }
 }
