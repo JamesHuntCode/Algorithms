308e4a3c4902e30c292c1d96715d1fc29b1502ad
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-25 22:59:34 +0100

    different sorting algorithms to tackle laid out for implemenation

--- Sorting Algorithms/SortingAlgorithmExamples/SortingAlgorithmExamples/Program.cs
+++ Sorting Algorithms/SortingAlgorithmExamples/SortingAlgorithmExamples/Program.cs
@@ -8,7 +8,81 @@ namespace SortingAlgorithmExamples
 {
     class Program
     {
+        /// <summary>
+        /// Program to demonstrate the implementation of a few different and popular ways of sorting an unordered dataset. 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
+        {
+
+        }
+
+        /// <summary>
+        /// Selection sort.
+        /// </summary>
+        /// <param name="unsortedArray"></param>
+        /// <returns></returns>
+        static int[] SelectionSort(int[] unsortedArray)
+        {
+
+        }
+
+        /// <summary>
+        /// Bubble sort.
+        /// </summary>
+        /// <param name="unsortedArray"></param>
+        /// <returns></returns>
+        static int[] BubbleSort(int[] unsortedArray)
+        {
+
+        }
+
+        /// <summary>
+        /// Insertion sort.
+        /// </summary>
+        /// <param name="unsortedArray"></param>
+        /// <returns></returns>
+        static int[] InsertionSort(int[] unsortedArray)
+        {
+
+        }
+
+        /// <summary>
+        /// Merge sort.
+        /// </summary>
+        /// <param name="unsortedArray"></param>
+        /// <returns></returns>
+        static int[] MergeSort(int[] unsortedArray)
+        {
+
+        }
+
+        /// <summary>
+        /// Quick sort.
+        /// </summary>
+        /// <param name="unsortedArray"></param>
+        /// <returns></returns>
+        static int[] QuickSort(int[] unsortedArray)
+        {
+
+        }
+
+        /// <summary>
+        /// Counting sort.
+        /// </summary>
+        /// <param name="unsortedArray"></param>
+        /// <returns></returns>
+        static int[] CountingSort(int[] unsortedArray)
+        {
+
+        }
+
+        /// <summary>
+        /// Heap sort.
+        /// </summary>
+        /// <param name="unsortedArray"></param>
+        /// <returns></returns>
+        static int[] HeapSort(int[] unsortedArray)
         {
 
         }
