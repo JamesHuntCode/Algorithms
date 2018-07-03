72042407c52f076cf3e1503ea06b90a2f4673903
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-07-03 02:30:36 +0100

    insertion sort success / new method added / corrected bubble sort

--- Sorting Algorithms/SortingAlgorithmExamples/SortingAlgorithmExamples/Program.cs
+++ Sorting Algorithms/SortingAlgorithmExamples/SortingAlgorithmExamples/Program.cs
@@ -17,18 +17,7 @@ namespace SortingAlgorithmExamples
             // Log the sorted arrays & the initial dataset:
             Console.WriteLine("\nOriginal dataset: \n");
 
-            int[] mixedNumbers = new int[10];
-
-            mixedNumbers[0] = 5;
-            mixedNumbers[1] = 7;
-            mixedNumbers[2] = 1;
-            mixedNumbers[3] = 11;
-            mixedNumbers[4] = 14;
-            mixedNumbers[5] = 3;
-            mixedNumbers[6] = 2;
-            mixedNumbers[7] = 9;
-            mixedNumbers[8] = 10;
-            mixedNumbers[9] = 17;
+            int[] mixedNumbers = GetRandomArray();   
 
             for (int i = 0; i < mixedNumbers.Length; i++)
             {
@@ -37,19 +26,19 @@ namespace SortingAlgorithmExamples
 
             // Different sorting methods:
             Console.WriteLine("\n\nSelection sort - O(n^2):\n");
-            LogElements(SelectionSort(mixedNumbers));
+            LogElements(SelectionSort(GetRandomArray()));
             Console.WriteLine("\n\nBubble sort - O(n^2):\n");
-            LogElements(BubbleSort(mixedNumbers));
-            Console.WriteLine("\n\nInsertion sort: \n");
-            LogElements(InsertionSort(mixedNumbers));
+            LogElements(BubbleSort(GetRandomArray()));
+            Console.WriteLine("\n\nInsertion sort - O(n^2):\n");
+            LogElements(InsertionSort(GetRandomArray()));
             Console.WriteLine("\n\nMerge sort: \n");
-            LogElements(MergeSort(mixedNumbers));
+            LogElements(MergeSort(GetRandomArray()));
             Console.WriteLine("\n\nQuick sort: \n");
-            LogElements(QuickSort(mixedNumbers));
+            LogElements(QuickSort(GetRandomArray()));
             Console.WriteLine("\n\nCounting sort: \n");
-            LogElements(CountingSort(mixedNumbers));
+            LogElements(CountingSort(GetRandomArray()));
             Console.WriteLine("\n\nHeap sort: \n");
-            LogElements(HeapSort(mixedNumbers));
+            LogElements(HeapSort(GetRandomArray()));
 
             // Program finished.
             Console.Read();
@@ -58,6 +47,7 @@ namespace SortingAlgorithmExamples
         /// <summary>
         /// Selection sort.
         /// O(n^2) time complexity.
+        /// Two 'halves', pick smallest element from right and insert it into the left.
         /// </summary>
         /// <param name="array"></param>
         /// <returns></returns>
@@ -92,6 +82,7 @@ namespace SortingAlgorithmExamples
         /// <summary>
         /// Bubble sort.
         /// O(n^2) time complexity.
+        /// Pick two elements at a time, compare them and put the smaller to the left. Repeat this until no shifts are required.
         /// </summary>
         /// <param name="array"></param>
         /// <returns></returns>
@@ -103,7 +94,7 @@ namespace SortingAlgorithmExamples
             for (int i = 0; i < (length - 1); i++)
             {
                 // Compare all indexes with the index one shift to the right.
-                for (int j = 0; j < (j - i - length); j++)
+                for (int j = 0; j < (length - i - 1); j++)
                 {
                     // Interchange them if the left is smaller than the right.
                     if (array[j] > array[j + 1])
@@ -120,13 +111,30 @@ namespace SortingAlgorithmExamples
 
         /// <summary>
         /// Insertion sort.
+        /// O(n^2) time complexity.
+        /// Move across the array, insert the next element into the correct index by shifting the other elements.
         /// </summary>
         /// <param name="array"></param>
         /// <returns></returns>
         static int[] InsertionSort(int[] array)
         {
+            int length = array.Length;
 
+            // Iterate over the array starting at the second index.
+            for (int i = 1; i < length; ++i)
+            {
+                int key = array[i];
+                int j = (i - 1);
 
+                // Move elements that are greater than key one index to the right.
+                while ((j >= 0) && (array[j] > key))
+                {
+                    array[j + 1] = array[j];
+                    j = (j - 1);
+                }
+
+                array[j + 1] = key;
+            }
 
             return array;
         }
@@ -181,6 +189,28 @@ namespace SortingAlgorithmExamples
             {
                 Console.Write(array[i].ToString() + " ");
             }
+        }
+
+        /// <summary>
+        /// Method to return an array of random integer values.
+        /// </summary>
+        /// <returns></returns>
+        static int[] GetRandomArray()
+        {
+            int[] mixedNumbers = new int[10];
+
+            mixedNumbers[0] = 5;
+            mixedNumbers[1] = 7;
+            mixedNumbers[2] = 1;
+            mixedNumbers[3] = 11;
+            mixedNumbers[4] = 14;
+            mixedNumbers[5] = 3;
+            mixedNumbers[6] = 2;
+            mixedNumbers[7] = 9;
+            mixedNumbers[8] = 10;
+            mixedNumbers[9] = 17;
+
+            return mixedNumbers;
         }
     }
 }
